3bfcb27f3767d9eee66c04b7ba8316782f91be2c
Ghezlene Doballah <ghyzounette@gmail.com>
2020-02-04 16:55:33 +0100

    print-holiday

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var thisMonth = 1;
+var thisMonth = 0;
 var year = 2018;
 var monthObject = moment(
   {
@@ -8,7 +8,8 @@ var monthObject = moment(
     month:thisMonth
   }
 );
-printMonth(monthObject)
+printMonth(monthObject);
+printHoliday(monthObject);
 
 });
 
@@ -16,15 +17,57 @@ printMonth(monthObject)
 function printMonth(month) {
   var daysInMonth = month.daysInMonth();
   for (var i = 1; i <= daysInMonth; i++) {
-    console.log(i);
-    var source = $('#entry-template');
+    // console.log(i);
+    var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
     var context = {
-      number: i,
-      month: month.format('MMMM')
-   };
+        number: i,
+        month: month.format('MMMM'),
+        date: month.format('YYYY-MM') + '-' + addZero(i)
+        };
     var html = template(context);
-    $('.calender').append(html);
+    $('.calendar').append(html);
 
   }
+};
+function printHoliday(month) {
+  $.ajax(
+    {
+    url : 'https://flynn.boolean.careers/exercises/api/holidays',
+    method : 'GET',
+    data : {
+      year : month.year(),
+      month : month.month()
+    },
+    success: function (data) {
+      var holidays = data.response;
+      console.log(holidays);
+      for (var i = 0; i < holidays.length; i++) {
+        var holiday = holidays[i].date;
+        console.log(holiday);
+        $('.day').each(function () {
+          console.log($(this).attr('data-date'));
+          if ($(this).attr('data-date') == holiday) {
+            console.log($(this));
+          $(this).addClass('red');
+          $(this).find('.holiday-name').append(holidays[i].name);
+
+          }
+        });
+      }
+
+    },
+    error : function (request, state, errors) {
+      alert('Errore ' + errors);
+    }
+
+  });
+
+};
+
+function addZero(num) {
+  if(num < 10) {
+    return '0' + num;
+  }
+  return num;
 }
